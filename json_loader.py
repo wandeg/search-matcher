6360932a0665c77d5e9ff7d8cafd82d542f32e20
James Maringa <jmsmaringa@gmail.com>
2015-03-05 00:01:24 +0300

    Setting up indexes

--- json_loader.py
+++ json_loader.py
@@ -9,3 +9,7 @@ with open('people.json', 'r') as f:
     people_data = json.load(f)
     people = people_data['result']
     db.people.insert(people)
+db.people.create_index('age')
+db.people.create_index('gender')
+db.people.create_index('company')
+db.people.create_index('tags')
